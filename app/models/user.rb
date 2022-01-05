13fa9bf7f82ed77d3d07800087201a576519fbf7
AGE1997 <baseball.0331e.t@gmail.com>
2022-01-05 21:38:23 +0900

    userモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
+  validates :nickname,             presence: true
+  validates :last_name,            presence: true
+  validates :first_name,           presence: true
+  validates :pseudonym_last_name,  presence: true
+  validates :pseudonym_first_name, presence: true
+  validates :birthday,             presence: true
 end
