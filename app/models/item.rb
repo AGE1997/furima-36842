5e515f3a5cfbb756b0f598e729da9d12c860cb7c
AGE1997 <baseball.0331e.t@gmail.com>
2022-01-12 20:56:31 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,8 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :condition, :shipping_charges, :prefecture, :days
+
+  validates :image, :name, :explanation, :price, presence: true
+
+  validates :category_id, :condition_id, :shipping_charges_id, :prefecture_id, :days_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
