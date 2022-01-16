74f47c377b44d4035cfbcbfdbe0e2e2f9438a74b
AGE1997 <baseball.0331e.t@gmail.com>
2022-01-16 19:35:24 +0900

    商品出品ページの表示と保存

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,10 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :condition, :shipping_charges, :prefecture, :days
 
-  validates :image, :name, :explanation, :price, presence: true
+  with_options presence: true do
+    validates :image, :name, :explanation
+    validates :price, inclusion: { in: 300..9_999_999 , message: "is out of setting range"}, format: { with: /\A[0-9]+\z/ , message: "is invalid. Input half-width characters"}
+  end
 
   validates :category_id, :condition_id, :shipping_charges_id, :prefecture_id, :days_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
