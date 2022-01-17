a27535ae8afa846d271b4d4798ccee1fc9ed22ce
AGE1997 <baseball.0331e.t@gmail.com>
2022-01-18 02:33:20 +0900

    itemモデルのテストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,77 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品' do
+    context '商品が出品できる場合' do
+      it 'image、name、explanation、category、condition、sihpping_charges、prefecture、days、priceが存在していれば保存できる' do
+        expect(@item).to be_valid
+      end
+    end
+    context '商品が出品できない場合' do
+      it  'imageが空では保存できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'nameが空では保存できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'explanationが空では保存できない' do
+        @item.explanation = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Explanation can't be blank")
+      end
+      it 'categoryが空では保存できない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'conditionが空では保存できない' do
+        @item.condition_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it 'shipping_chargesが空では保存できない' do
+        @item.shipping_charges_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping charges can't be blank")
+      end
+      it 'prefectureが空では保存できない' do
+        @item.prefecture_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it 'daysが空では保存できない' do
+        @item.days_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Days can't be blank")
+      end
+      it 'priceが空では保存できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'priceは300~9,999,999の間でないと保存できない' do
+        @item.price = 111
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is out of setting range")
+      end
+      it 'priceは半角数値でないと保存できない' do
+        @item.price = '３３１'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is out of setting range")
+      end
+      it 'userが紐づいていないと保存できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
