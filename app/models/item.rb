7480498ea0eec24ca086433c043f7a98a21f9349
AGE1997 <baseball.0331e.t@gmail.com>
2022-01-22 20:55:38 +0900

    商品一覧の表示の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category 
+  belongs_to :category
   belongs_to :condition
   belongs_to :shipping_charges
   belongs_to :prefecture
